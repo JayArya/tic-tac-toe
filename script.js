0fb300e461e5d16db154198af579fc3807d1e0c2
JayArya <spice.arya@gmail.com>
2022-06-22 22:25:37 +0530

    Implemet MVP
    Implement basic playable game

---
+++ script.js
@@ -0,0 +1,91 @@
+const finalResultModal = document.querySelector(".final-result-modal");
+let moveCounter = 0;
+const Gameboard = (() => {
+  const gameboard = [];
+  for (let i = 0; i < 9; i++) {
+    gameboard[i] = i;
+  }
+  let winner = null;
+
+  const resetBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      gameboard[i] = i;
+      document.querySelector(`[data-grid-cell="${i}"]`).innerHTML = "";
+    }
+    winner = null;
+  };
+  // resetBoard();
+
+  const checkResult = () => {
+    if (gameboard[0] === gameboard[3] && gameboard[0] === gameboard[6])
+      winner = gameboard[0];
+    else if (gameboard[1] === gameboard[4] && gameboard[1] === gameboard[7])
+      winner = gameboard[1];
+    else if (gameboard[2] === gameboard[5] && gameboard[2] === gameboard[8])
+      winner = gameboard[2];
+    else if (gameboard[0] === gameboard[1] && gameboard[0] === gameboard[2])
+      winner = gameboard[0];
+    else if (gameboard[3] === gameboard[4] && gameboard[3] === gameboard[5])
+      winner = gameboard[3];
+    else if (gameboard[6] === gameboard[7] && gameboard[6] === gameboard[8])
+      winner = gameboard[6];
+    else if (gameboard[0] === gameboard[4] && gameboard[0] === gameboard[8])
+      winner = gameboard[0];
+    else if (gameboard[2] === gameboard[4] && gameboard[2] === gameboard[6])
+      winner = gameboard[2];
+  };
+  const updateBoard = (index, symbol) => {
+    if (
+      gameboard[index] === "X" ||
+      gameboard[index] === "O" ||
+      winner !== null
+    ) {
+      return;
+    } else {
+      gameboard[index] = symbol;
+      document.querySelector(`[data-grid-cell="${index}"]`).innerHTML = symbol;
+      moveCounter++;
+      checkResult();
+      if (winner !== null) {
+        const finalResult = document.querySelector(".final-result");
+        finalResult.textContent = `${winner} wins 🎉`;
+        finalResultModal.style.display = "flex";
+      }
+    }
+  };
+  return { winner, resetBoard, updateBoard };
+})();
+
+const Player = (name, symbol) => {
+  const playerName = name;
+  const playerSymbol = symbol;
+
+  return {};
+};
+
+const playAgain = () => {
+  moveCounter = 0;
+  Gameboard.resetBoard();
+  finalResultModal.style.display = "none";
+};
+
+const player1 = Player("Jayant", "X");
+const player2 = Player("Computer", "O");
+
+const gameGrid = document.getElementById("game-grid");
+
+for (let i = 0; i < 9; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("game-grid-cell");
+  cell.dataset.gridCell = i;
+  cell.addEventListener("click", (e) => {
+    if (moveCounter < 9) {
+      if (moveCounter % 2 === 0) {
+        Gameboard.updateBoard(e.target.dataset.gridCell, "X");
+      } else {
+        Gameboard.updateBoard(e.target.dataset.gridCell, "O");
+      }
+    }
+  });
+  gameGrid.appendChild(cell);
+}
